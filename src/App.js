6ecbc9d10dace5dc78df7ce873afb645a9e824cb
Gatekeeper-joe <investorjk5x@gmail.com>
2024-02-22 16:59:10 +0900

    component作成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Child from './components/Child/Child';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Child />
     </div>
   );
 }
